c89fbd6b7fa9de27ced9aa65d135240201d60887
Andrei Ababei <ababeiandrei94@gmail.com>
2026-05-03 13:12:36 +0300

    Added token migration

---
+++ src/Runners/Persistence/Migrations/20260503091239_AddedTokenColumn.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Runners.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedTokenColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Token",
+                table: "RunnerItems",
+                type: "TEXT",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Token",
+                table: "RunnerItems");
+        }
+    }
+}
